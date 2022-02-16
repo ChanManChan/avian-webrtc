13bde547c843b819b7247a4c6cff01c515a8da04
ChanManChan <ngopal253@gmail.com>
2022-02-16 16:49:20 +0530

    landing-page-base-design-complete

---
+++ public/assets/js/action.js
@@ -0,0 +1,15 @@
+$("#joinMeetingNavBtn").click(() => {
+    $(".code-input").focus()
+})
+
+$("#joinBtn").click(() => {
+    const meetingCode = $(".code-input").val().trim()
+    const meetingUrl = window.location.origin + "?meetingId=" + meetingCode
+    window.location.href = meetingUrl
+})
+
+$(".newMeetingBtn").click(() => {
+    const meetingCode = Math.floor(Math.random() * 100000000)
+    const meetingUrl = window.location.origin + "?meetingId=" + meetingCode
+    window.location.href = meetingUrl
+})
