5f7790889e6a84ff66d85b93f2098229fd90711c
ChanManChan <ngopal253@gmail.com>
2022-02-17 23:53:20 +0530

    socket-user-awareness-init

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,13 @@
-let socket
+let connected = false
+const socket = io("http://localhost:3000")
 const urlParams = new URLSearchParams(window.location.search)
 const meetingId = urlParams.get("meetingId")
 const userId = window.prompt("Enter your username")
+const meetingData = {
+    userId,
+    meetingId
+}
+let connectedUsers = []
 
 if (!userId || !meetingId) {
     alert("Username or meeting id is missing")
@@ -15,8 +21,17 @@ function init(userId, meetingId) {
 }
 
 function eventProcessForSignalingServer() {
-    socket = io.connect()
-    socket.on("connect", () => {
-        alert("socket connected on client side")
+    socket.emit("setup", meetingData)
+    socket.on("connected", existingUsers => {
+        connectedUsers = [...existingUsers]
+        connected = true
     })
+    socket.on("new node", nodeData => {
+        connectedUsers.push(nodeData)
+        addUser({ ...nodeData })
+    })
+}
+
+function addUser({ connectionId, userId, meetingId }) {
+
 }
--- server.js
+++ server.js
@@ -4,10 +4,26 @@ const path = require('path')
 const app = express()
 const PORT = 3000
 const server = app.listen(PORT, () => console.log("Listening on port " + PORT))
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, { pingTimeout: 60000 })
 
 app.use(express.static(path.join(__dirname, "./public")))
+const userConnections = []
 
-io.on("connect", socket => {
-    console.log("socket id is ", socket.id)
+io.on("connection", socket => {
+
+    socket.on("setup", meetingData => {
+        const newNode = {
+            connectionId: socket.id,
+            userId: meetingData.userId,
+            meetingId: meetingData.meetingId
+        }
+
+        // inform others in the same room about the new connection
+        const existingUsers = userConnections.filter(u => u.meetingId == meetingData.meetingId)
+        existingUsers.forEach(u => socket.to(u.connectionId).emit("new node", newNode))
+
+        userConnections.push(newNode)
+        socket.emit("connected", existingUsers)
+    })
+
 })
